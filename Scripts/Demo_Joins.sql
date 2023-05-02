b6f68dd0498c75474cb11e3919a016dac26cfbbb
eduit-learning <achavez@grupoeduit.com>
2023-05-02 12:30:03 -0600

    Ejercicio en clase

--- Scripts/Demo_Joins.sql
+++ Scripts/Demo_Joins.sql
@@ -48,3 +48,125 @@ ON a.nombre = b.nombre
 SELECT * FROM Conjunto_A AS a LEFT JOIN Conjunto_B AS b
 ON a.nombre = b.nombre
 WHERE b.nombre IS NULL
+
+---------------------Clase martes 2 de mayo de 2023-----------------------
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID = 20
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID = 20
+
+ALTER TABLE dbo.Conjunto_B
+ADD CONSTRAINT [FK_CB-CA] FOREIGN KEY (conjunto_AID)
+REFERENCES dbo.Conjunto_A (conjunto_AID)
+
+--eSTE QUERY ES LO MISMO QUE EL DE ARRIBA
+ALTER TABLE dbo.Conjunto_B
+ADD CONSTRAINT [FK_CB-CA] FOREIGN KEY (conjunto_AID)
+REFERENCES dbo.Conjunto_A (conjunto_AID)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID = 20
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID = 20
+
+ALTER TABLE Conjunto_B
+NOCHECK CONSTRAINT [FK_CB-CA]
+
+UPDATE Conjunto_A SET conjunto_AID = 10000
+WHERE conjunto_AID = 20
+
+SELECT * FROM Conjunto_A WHERE conjunto_AID IN (20, 10000)
+SELECT * FROM Conjunto_B WHERE conjunto_AID IN (20, 10000)
+
+DELETE Conjunto_A WHERE conjunto_AID = 10000
+
+ALTER TABLE Conjunto_B
+CHECK CONSTRAINT [FK_CB-CA]
+
+ALTER TABLE dbo.Conjunto_B
+DROP CONSTRAINT [FK_CB-CA]
+
+--------------------------------------------------------------------
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID = 22
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID = 22
+
+DELETE FROM Conjunto_B WHERE conjunto_AID = 20
+
+ALTER TABLE dbo.Conjunto_B
+ADD CONSTRAINT [FK_CB-CA] FOREIGN KEY (conjunto_AID)
+REFERENCES dbo.Conjunto_A (conjunto_AID)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+
+UPDATE Conjunto_A SET conjunto_AID = 10000
+WHERE conjunto_AID = 21
+
+DELETE Conjunto_A WHERE conjunto_AID = 10000
+
+UPDATE Conjunto_B SET conjunto_AID = 10000
+WHERE conjunto_AID = 22
+
+DELETE Conjunto_B WHERE conjunto_AID = 22
+
+ALTER TABLE dbo.Conjunto_B
+DROP CONSTRAINT [FK_CB-CA]
+--------------------------------------------------------------
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID = 23
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID = 23
+
+ALTER TABLE dbo.Conjunto_B
+ADD CONSTRAINT [FK_CB-CA] FOREIGN KEY (conjunto_AID)
+REFERENCES dbo.Conjunto_A (conjunto_AID)
+ON DELETE SET NULL
+ON UPDATE SET NULL
+
+ALTER TABLE Conjunto_B ALTER COLUMN conjunto_AID int NULL
+
+UPDATE Conjunto_A SET conjunto_AID = 10000
+WHERE conjunto_AID = 23
+
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID IN (10000, 24)
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID IS NULL OR conjunto_AID = 24
+
+DELETE Conjunto_A WHERE conjunto_AID = 24
+
+DELETE Conjunto_B WHERE conjunto_AID IS NULL
+DELETE Conjunto_A WHERE conjunto_AID = 10000
+
+ALTER TABLE dbo.Conjunto_B
+DROP CONSTRAINT [FK_CB-CA]
+
+ALTER TABLE Conjunto_B
+ALTER COLUMN conjunto_AID int NOT NULL
+
+-------------------------------------------------------------
+SELECT * FROM Conjunto_A
+WHERE conjunto_AID = 10000
+SELECT * FROM Conjunto_B
+WHERE conjunto_AID = 10000
+
+ALTER TABLE Conjunto_B
+ADD CONSTRAINT [DEFAULT(conjunto_AID)]
+DEFAULT 10000 FOR conjunto_AID
+
+ALTER TABLE dbo.Conjunto_B
+ADD CONSTRAINT [FK_CB-CA] FOREIGN KEY (conjunto_AID)
+REFERENCES dbo.Conjunto_A (conjunto_AID)
+ON DELETE SET DEFAULT
+ON UPDATE SET DEFAULT
+
+UPDATE Conjunto_A SET conjunto_AID = 10000
+WHERE conjunto_AID = 25
+
+DELETE Conjunto_A WHERE conjunto_AID = 26
+
+ALTER TABLE Conjunto_B
+ALTER COLUMN conjunto_AID int NOT NULL
