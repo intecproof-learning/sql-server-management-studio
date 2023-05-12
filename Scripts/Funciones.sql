8bb4bdcfe35e5aeff740f55b7a6601153c2fcedd
eduit-learning <achavez@grupoeduit.com>
2023-05-12 14:41:49 -0600

    Update Funciones.sql

--- Scripts/Funciones.sql
+++ Scripts/Funciones.sql
@@ -19,6 +19,7 @@ CREATE FUNCTION ufn_ConcatName
 RETURNS nvarchar(150)
 AS
 BEGIN
+
 RETURN (SELECT CONCAT(@firstName, @middleName, @lastName))
 END
 GO
@@ -29,3 +30,32 @@ dbo.ufn_ConcatName(FirstName, MiddleName, LastName) AS FullName,
 BusinessEntityID,
 PersonType
 FROM Person.Person
+
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+
+EXEC sp_configure 'clr enable', 1;
+RECONFIGURE;
+
+EXEC sp_configure 'clr strict security', 0;
+RECONFIGURE;
+
+CREATE ASSEMBLY SQLCLRDemo2
+FROM 'C:\Temp\CLRFunctionsSQL.dll'
+GO
+
+CREATE FUNCTION ObtenerHoraActualISO()
+RETURNS nvarchar(20)
+EXTERNAL NAME SQLCLRDemo2.SQLFunctions.ObtenerHoraActualISO
+
+SELECT dbo.ObtenerHoraActualISO()
+
+ALTER ASSEMBLY SQLCLRDemo2
+FROM 'C:\Temp\CLRFunctionsSQL.dll'
+GO
+
+CREATE FUNCTION PrimerCaracter(@str nvarchar(300))
+RETURNS int
+EXTERNAL NAME SQLCLRDemo2.SQLFunctions.PrimerCaracter
+
+SELECT dbo.PrimerCaracter('asdfadsfasdfaasdga')
