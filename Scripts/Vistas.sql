b31497c8ec81ef0c449390de5a7aab570182c7ed
eduit-learning <achavez@grupoeduit.com>
2023-05-29 14:39:02 -0600

    Create Vistas.sql

---
+++ Scripts/Vistas.sql
@@ -0,0 +1,80 @@
+CREATE PROCEDURE CrearTablaTrabajo
+AS
+BEGIN
+	DROP TABLE dbo.Resultados
+	CREATE TABLE dbo.Resultados
+	(
+		id int NOT NULL PRIMARY KEY,
+		tipo nvarchar(50) NOT NULL,
+		resultado int NOT NULL
+	)
+	
+	INSERT INTO Resultados (id, tipo, resultado)
+	VALUES (1, 'Tipo 1', 2),
+	(2, 'Tipo 1', 2),
+	(3, 'Tipo 1', 2),
+	(4, 'Tipo 2', 2),
+	(5, 'Tipo 2', 2),
+	(6, 'Tipo 2', 2),
+	(7, 'Tipo 1', 2),
+	(8, 'Tipo 1', 2),
+	(9, 'Tipo 2', 2),
+	(10, 'Tipo 2', 2),
+	(11, 'Tipo 3', 2),
+	(12, 'Tipo 3', 2),
+	(13, 'Tipo 3', 2),
+	(14, 'Tipo 3', 2),
+	(15, 'Tipo 3', 2),
+	(16, 'Tipo 4', 2),
+	(17, 'Tipo 4', 2),
+	(18, 'Tipo 4', 2),
+	(19, 'Tipo 4', 2),
+	(20, 'Tipo 4', 2),
+	(21, 'Tipo 5', 2),
+	(22, 'Tipo 5', 2),
+	(23, 'Tipo 5', 2),
+	(24, 'Tipo 5', 2),
+	(25, 'Tipo 5', 2)
+END
+GO
+
+EXEC CrearTablaTrabajo
+GO
+
+CREATE VIEW VistaNormal
+AS
+	SELECT id, tipo, resultado
+	FROM dbo.Resultados
+GO
+
+SELECT * FROM VistaNormal
+GO
+
+ALTER TABLE dbo.Resultados DROP COLUMN resultado
+GO
+
+SELECT * FROM dbo.Resultados
+GO
+
+DROP VIEW VistaNormal
+GO
+
+EXEC CrearTablaTrabajo
+GO
+
+----------------------------SCHEMABINDING
+CREATE VIEW VistaSchemaBinding
+WITH SCHEMABINDING
+AS
+	SELECT id, tipo, resultado
+	FROM dbo.Resultados
+GO
+
+SELECT * FROM VistaSchemaBinding
+GO
+
+SELECT * FROM dbo.Resultados
+GO
+
+ALTER TABLE dbo.Resultados DROP COLUMN resultado
+GO
