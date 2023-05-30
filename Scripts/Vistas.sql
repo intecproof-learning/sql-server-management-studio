94dca2e1cf37c5c368dce2ed3a851b247a07cd79
eduit-learning <achavez@grupoeduit.com>
2023-05-30 11:38:03 -0600

    Update Vistas.sql

--- Scripts/Vistas.sql
+++ Scripts/Vistas.sql
@@ -78,3 +78,47 @@ GO
 
 ALTER TABLE dbo.Resultados DROP COLUMN resultado
 GO
+------------------------------CHECL OPTION
+EXEC CrearTablaTrabajo
+GO
+
+CREATE VIEW VistaSchemaBindingEdit
+WITH SCHEMABINDING
+AS
+	SELECT id, tipo, resultado
+	FROM dbo.Resultados
+GO
+
+SELECT * FROM VistaSchemaBindingEdit
+GO
+
+INSERT INTO VistaSchemaBindingEdit (id, tipo, resultado)
+VALUES(26, 'Tipo 6', 2)
+GO
+
+UPDATE VistaSchemaBindingEdit SET resultado = 3
+GO
+
+ALTER VIEW VistaSchemaBindingEdit
+WITH SCHEMABINDING
+AS
+	SELECT id, tipo
+	FROM dbo.Resultados
+GO
+
+INSERT INTO VistaSchemaBindingEdit (id, tipo)
+VALUES(27, 'Tipo 6')
+GO
+
+UPDATE VistaSchemaBindingEdit SET resultado = 2
+GO
+
+SELECT * FROM VistaSchemaBindingEdit
+GO
+
+SELECT * FROM dbo.Resultados
+GO
+
+ALTER TABLE dbo.Resultados ADD CONSTRAINT [DF_Resultados(resultado)]
+DEFAULT 2 FOR resultado
+GO
