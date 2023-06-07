cd52dda10770537f5c7739c62c0dfb916e4a88ac
eduit-learning <achavez@grupoeduit.com>
2023-06-07 11:30:11 -0600

    Create Backup_Timeline.sql

---
+++ Scripts/Backup_Timeline.sql
@@ -0,0 +1,26 @@
+--1.- Verificar que mi base de datos esté en modo recovery full
+--2.- Generar un backup completo
+BACKUP DATABASE AdventureWorks
+TO DISK = 'C:\Temp\AW_DR.bak'
+WITH FORMAT,
+MEDIANAME = 'Backup_Timeline_Simple',
+MEDIADESCRIPTION = 'Media que contiene backups en  modo simple',
+NAME = 'Primer_Respaldo',
+DESCRIPTION = 'Primer full backup'
+
+DECLARE @counter int = 0
+WHILE @counter < 5
+BEGIN
+	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+	('Mexico', 'Norte', @counter, GETDATE())
+
+	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+	('Mexico', 'Sur', @counter, GETDATE())
+
+	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+	('Mexico', 'Este', @counter, GETDATE())
+
+	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+	('Mexico', 'Oeste', @counter, GETDATE())
+
+END
