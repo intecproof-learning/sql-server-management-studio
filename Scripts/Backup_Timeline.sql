a487f8b8bf4fbcfc230d58a4236c409d098ef81c
eduit-learning <achavez@grupoeduit.com>
2023-06-07 17:41:14 -0600

    Update Backup_Timeline.sql

--- Scripts/Backup_Timeline.sql
+++ Scripts/Backup_Timeline.sql
@@ -3,13 +3,13 @@
 BACKUP DATABASE AdventureWorks
 TO DISK = 'C:\Temp\AW_DR.bak'
 WITH FORMAT,
-MEDIANAME = 'Backup_Timeline_Simple',
-MEDIADESCRIPTION = 'Media que contiene backups en  modo simple',
+MEDIANAME = 'Backup_Timeline',
+MEDIADESCRIPTION = 'Media que contiene backups en  modo full',
 NAME = 'Primer_Respaldo',
 DESCRIPTION = 'Primer full backup'
 
-DECLARE @counter int = 0
-WHILE @counter < 5
+DECLARE @counter int = 5
+WHILE @counter < 10
 BEGIN
 	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
 	('Mexico', 'Norte', @counter, GETDATE())
@@ -27,10 +27,12 @@ BEGIN
 	WAITFOR DELAY '00:02:00'
 END
 
-BACKUP DATABASE AdventureWorks
+BACKUP LOG AdventureWorks
 TO DISK = 'C:\Temp\AW_DR.bak'
 WITH
-MEDIANAME = 'Backup_Timeline_Simple',
-MEDIADESCRIPTION = 'Media que contiene backups en  modo simple',
+MEDIANAME = 'Backup_Timeline',
+MEDIADESCRIPTION = 'Media que contiene backups en  modo full',
 NAME = 'Segundo_Respaldo',
-DESCRIPTION = 'Segundo full backup'
+DESCRIPTION = 'Primer log backup'
+
+SELECT * FROM dbo.Sales
