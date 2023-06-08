7cd82bf39dda146f0c17e3b510cf44e8e73bd4f4
eduit-learning <achavez@grupoeduit.com>
2023-06-08 11:34:55 -0600

    Update Backup_Timeline.sql

--- Scripts/Backup_Timeline.sql
+++ Scripts/Backup_Timeline.sql
@@ -8,8 +8,8 @@ MEDIADESCRIPTION = 'Media que contiene backups en  modo full',
 NAME = 'Primer_Respaldo',
 DESCRIPTION = 'Primer full backup'
 
-DECLARE @counter int = 5
-WHILE @counter < 10
+DECLARE @counter int = 10
+WHILE @counter < 15
 BEGIN
 	INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
 	('Mexico', 'Norte', @counter, GETDATE())
@@ -35,4 +35,35 @@ MEDIADESCRIPTION = 'Media que contiene backups en  modo full',
 NAME = 'Segundo_Respaldo',
 DESCRIPTION = 'Primer log backup'
 
-SELECT * FROM dbo.Sales
+SET @counter = 15
+WHILE @counter < 20
+BEGIN
+	BEGIN TRANSACTION Insert_Norte_Sur
+	WITH MARK 'Insercion de ventas en Mexico Norte y Sur'
+		INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+		('Mexico', 'Norte', @counter, GETDATE())
+
+		INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+		('Mexico', 'Sur', @counter, GETDATE())
+	COMMIT TRANSACTION Insert_Norte_Sur
+
+	BEGIN TRANSACTION Insert_Este_Oeste
+	WITH MARK 'Insercion de ventas en Mexico Este y Oeste'
+		INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+		('Mexico', 'Este', @counter, GETDATE())
+
+		INSERT INTO dbo.Sales (country, region, sales, fecha) VALUES
+		('Mexico', 'Oeste', @counter, GETDATE())
+	COMMIT TRANSACTION Insert_Este_Oeste
+
+	SET @counter =	@counter + 1
+	WAITFOR DELAY '00:02:00'
+END
+
+BACKUP LOG AdventureWorks
+TO DISK = 'C:\Temp\AW_DR.bak'
+WITH
+MEDIANAME = 'Backup_Timeline',
+MEDIADESCRIPTION = 'Media que contiene backups en  modo full',
+NAME = 'Tercer_Respaldo',
+DESCRIPTION = 'Segundo log backup'
